d2c9691a64f0668417eaeffe110ea3bb7093141d
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2025-04-19 18:24:18 +0200

    Ajout d'une interface pour les tests locaux

---
+++ bot/interfaces.py
@@ -0,0 +1,39 @@
+from bot.abstract import AbstractInterface
+from chartichaud.game import Game
+
+position = tuple[int, int]
+
+class LocalInterface(AbstractInterface):
+    """
+    Interface locale entre le jeu et le bot.
+    """
+    def __init__(self, player: str, game: Game):
+        self.player = player
+        self.game = game
+
+    def build(self, pos: position, unit: str) -> None:
+        """
+        Crée une unité.
+        """
+        assert self.player == self.game.curPlayer
+        self.game.build(*pos, unit)
+    
+    def move(self, pos: position, newpos: position, unit: str) -> None:
+        """
+        Déplace une unité.
+        """
+        assert self.player == self.game.curPlayer
+        self.game.move(unit, *pos, *newpos)
+    
+    def farm(self, pos: position) -> None:
+        """
+        Récupère de l'or avec un citoyen.
+        """
+        assert self.player == self.game.curPlayer
+        self.game.farm(*pos)
+
+    def view(self) -> dict:
+        """
+        Renvoie les informations disponibles.
+        """
+        self.game.giveViewPlayer(self.player)
