f3d43bfb022548163d4cfe09e0d21fc11baaf668
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2025-04-20 17:45:24 +0200

    Maj de RandomBot

--- bot/randy.py
+++ bot/randy.py
@@ -1,5 +1,5 @@
 from bot.abstract import AbstractBot
-from random import choice
+from random import choice, shuffle
 
 class RandomBot(AbstractBot):
     def __init__(self, interface):
@@ -9,4 +9,22 @@ class RandomBot(AbstractBot):
         """
         Joue le tour.
         """
+        
+        # On récupère toutes les unités
+        allunits = []
+        for unit in ('C', 'M', 'B'):
+            units = self.getUnits(player=self.ownplayer, unit=unit)
+            for pos, value in units.items():
+                for _ in range(value):
+                    allunits.append((pos, unit))
+        shuffle(allunits)
+
+        # On fait agir tout le monde
+        while len(allunits) > 0:
+            pos, unit = allunits.pop()
+            moves = self.getAvailableMove(pos=pos, unit=unit)
+            self.play(move=choice(moves),
+                      pos=pos,
+                      unit=unit)
+        
         
