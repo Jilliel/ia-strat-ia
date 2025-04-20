e6e8eb6e8d5cf4aeef04caa6e57de7b3162c0ee9
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2025-04-20 21:08:42 +0200

    Ajout d'un match passant par l'API Flask web

--- scripts/match.py
+++ scripts/match.py
@@ -1,19 +1,23 @@
-from bot.interfaces import LocalInterface
+from bot.interfaces import LocalInterface, APIInterface
 from bot.abstract import AbstractBot
+
+from chartichaud.match import launchServer
 from chartichaud.game import Game
+
+from threading import Thread
 from time import time
 
 class LocalMatch:
     """
     Crée un match en local
     """
-    def __init__(self):
+    def __init__(self) -> None:
         self.game = Game()
         self.player1: AbstractBot = None
         self.player2: AbstractBot = None
         self.exectime: float = 0
 
-    def bind(self, bot1: AbstractBot, bot2: AbstractBot):
+    def bind(self, bot1: AbstractBot, bot2: AbstractBot) -> None:
         """
         Initialise les deux robots et leur interface.
         """
@@ -22,7 +26,7 @@ class LocalMatch:
         interface2 = LocalInterface(game=self.game, player='B')
         self.player2 = bot2(interface2)
 
-    def run(self):
+    def run(self) -> None:
         """
         Joue le match entre 
         """
@@ -48,7 +52,7 @@ class LocalMatch:
         t2 = time()
         self.exectime = t2-t1
 
-    def results(self):
+    def results(self) -> None:
         """
         Affiche les données du match.
         """
@@ -59,3 +63,41 @@ class LocalMatch:
         print(f"Match ended in {rounds} rounds / {self.exectime} seconds.")
         print(f"Player {winner} won the game with {scores[winner]} points.")
         print(f"Player {looser} lost the game with {scores[looser]} points.")
+
+
+class APIMatch:
+    """
+    Crée un match via un serveur web.
+    """
+    def __init__(self, port="8080") -> None:
+        self.game = Thread(target=launchServer, args=(port,))
+        self.game.daemon = True
+        self.game.start()
+        self.player1: AbstractBot = None
+        self.player2: AbstractBot = None
+
+    def bind(self, bot1: AbstractBot, bot2: AbstractBot) -> None:
+        """
+        Initialise les deux robots et leur interface.
+        """
+        interface1 = APIInterface(host='127.0.0.1', port='8080', name="bot1")
+        self.player1 = bot1(interface1)
+        interface2 = APIInterface(host='127.0.0.1', port='8080', name="bot2")
+        self.player2 = bot2(interface2)
+    
+    def run(self) -> None:
+        """
+        Joue le match entre 
+        """
+        thread1 = Thread(target=self.player1.playMatch)
+        thread1.daemon = True
+        thread2 = Thread(target=self.player2.playMatch)
+        thread2.daemon = True
+
+        # Lance les threads
+        thread1.start()
+        thread2.start()
+        
+        # Attend que les threads terminent
+        thread1.join()
+        thread2.join()
